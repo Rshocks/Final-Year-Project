f82c84b943fbd8307c74d6edbf252038b9dee3fe
ronan <rshocks297@gmail.com>
2022-10-29 10:43:36 +0100

    Update Stalled

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,5 +1,8 @@
 export default (posts = [], action) => {
     switch(action.type){
+        case 'UPDATE':
+            //mapping over posts array and changing something in array
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -36,8 +36,9 @@ export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     const post = req.body;
 
+    //to check if the id is valid using mongoose
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatePost = await PostMessage.findByIdAndUpdate(_id, post, { new: true});
-    res.json(updatePost);
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true});
+    res.json(updatedPost);
 }
